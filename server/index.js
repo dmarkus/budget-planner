02214f175b9e8f6702ad67b307b1f08de04bec57
Marek Dalka <mar.dalka@gmail.com>
2021-01-03 17:45:11 +0100

    Add Query and mutation, add start command

--- server/index.js
+++ server/index.js
@@ -1,4 +1,12 @@
-const { ApolloServer } = require('apollo-server');
+const { ApolloSconst typeDefs = require('./src/schema');erver } = require('apollo-server');
 const typeDefs = require('./src/schema');
 
 const server = new ApolloServer({ typeDefs });
+
+server.listen().then(() => {
+  console.log(`
+    Server is running!
+    Listening on port 4000
+    Explore at https://studio.apollographql.com/dev
+  `);
+});
--- server/src/schema.js
+++ server/src/schema.js
@@ -1,17 +1,33 @@
 const { gql } = require('apollo-server');
 
-type IncomeType {
-    name: String,
-}
-
-type IncomeItem {
-    id: ID!,
-    name: String
-}
-
 // TODO: add schema
 const typeDefs = gql`
-    
-`;
+    type Query {
+        incomeTypes: [IncomeType]!
+        monthlyIncomes(year: String, month: String): [IncomeItem]!
+        me: User
+    }
+
+    type Mutation {
+        login(email: String): User
+    }
+
+    type User {
+        id: ID!
+        email: String!
+        token: String
+    }
+
+    type IncomeType {
+        name: String,
+    }
+
+    type IncomeItem {
+        id: ID!,
+        name: String,
+        year: String, 
+        month: String
+    }
+ `;
 
 module.exports = typeDefs;
